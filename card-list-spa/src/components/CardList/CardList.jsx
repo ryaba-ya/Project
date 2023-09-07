6e6af34d8d61410a8bab8b9a61da1eaeb8932f82
ryaba-ya <ksushkenchik@bk.ru>
2023-09-07 10:33:15 +0200

    api

--- card-list-spa/src/components/CardList/CardList.jsx
+++ card-list-spa/src/components/CardList/CardList.jsx
@@ -5,45 +5,36 @@ import Card from '../Card/Card';
 import axios from 'axios';
 
 const CardList = () => {
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    // Функция для запроса данных из API
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://anime-facts-rest-api.herokuapp.com/api/v1/:anime_name'); // Замените URL на реальный API
-        setCards(response.data); // Сохраняем полученные данные в состоянии
-      } catch (error) {
-        console.error('Ошибка при получении данных из API:', error);
-      }
-    };
-
-    fetchData(); // Вызываем функцию при монтировании компонента
-  }, []);
-
-  // Обработчик для лайка карточки
-  const handleLikeClick = (cardId) => {
-    // Реализуйте логику лайка
+    const [cards, setCards] = useState([]);
+  
+    useEffect(() => {
+      // Функция для запроса данных из API
+      const fetchData = async () => {
+        try {
+          const response = await axios.get('https://anime-facts-rest-api.herokuapp.com/api/v1'); // Замените на корректный URL
+          setCards(response.data.data); // Обновляем состояние cards с данными из API
+        } catch (error) {
+          console.error('Ошибка при получении данных из API:', error);
+        }
+      };
+  
+      fetchData(); // Вызываем функцию при монтировании компонента
+    }, []);
+  
+    // ... (Ваши обработчики лайков и удаления)
+  
+    return (
+      <div>
+        {cards.map((card) => (
+          <Card
+            key={card.anime_id} // Используйте уникальный ключ
+            cardData={card}
+            onLikeClick={() => handleLikeClick(card.anime_id)}
+            onDeleteClick={() => handleDeleteClick(card.anime_id)}
+          />
+        ))}
+      </div>
+    );
   };
-
-  // Обработчик для удаления карточки
-  const handleDeleteClick = (cardId) => {
-    // Реализуйте логику удаления
-  };
-
-  return (
-    <div>
-      {cards.map((card) => (
-        <Card
-          key={card.id}
-          cardData={card}
-          onLikeClick={() => handleLikeClick(card.id)}
-          onDeleteClick={() => handleDeleteClick(card.id)}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default CardList;
-
+  
+  export default CardList;
